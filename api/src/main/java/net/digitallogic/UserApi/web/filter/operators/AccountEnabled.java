c1b62712d75257afb4e7579cefe850f27349ee62
lbrown <lbrown@digital-logic.net>
2022-03-11 14:31:42 -0700

    Filter based on provided date.

--- api/src/main/java/net/digitallogic/UserApi/web/filter/operators/AccountEnabled.java
+++ api/src/main/java/net/digitallogic/UserApi/web/filter/operators/AccountEnabled.java
@@ -25,9 +25,9 @@ public class AccountEnabled extends Operator<UserEntity, LocalDateTime> {
 
 		return builder.and(
 			builder.equal(join.get(UserStatusEntity_.accountEnabled), true),
-				builder.lessThanOrEqualTo(join.get(BiTemporalEntity_.id).get(BiTemporalEntityId_.validStart), LocalDateTime.now(Clock.systemUTC())),
+				builder.lessThanOrEqualTo(join.get(BiTemporalEntity_.id).get(BiTemporalEntityId_.validStart), args.get(0)),
 				builder.greaterThan(join.get(BiTemporalEntity_.validStop), LocalDateTime.now(Clock.systemUTC())),
-				builder.lessThanOrEqualTo(join.get(BiTemporalEntity_.id).get(BiTemporalEntityId_.systemStart), LocalDateTime.now(Clock.systemUTC())),
+				builder.lessThanOrEqualTo(join.get(BiTemporalEntity_.id).get(BiTemporalEntityId_.systemStart), args.get(0)),
 				builder.greaterThan(join.get(BiTemporalEntity_.systemStop), LocalDateTime.now(Clock.systemUTC()))
 		);
 	}
